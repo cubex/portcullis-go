428c890ff6fabddc809d09ff91a5cc59bf358796
Brooke Bryan <brooke@bajb.net>
2017-08-15 19:52:45 +0100

    Built in roles

---
+++ roles.go
@@ -0,0 +1,15 @@
+package portcullis
+
+//BuiltInRole Built in Kubex Roles
+type BuiltInRole string
+
+const (
+	// RoleProjectOwner project owner role
+	RoleProjectOwner BuiltInRole = "owner"
+	// RoleProjectMember project member
+	RoleProjectMember BuiltInRole = "member"
+	// RoleProjectViewer project viewer - read only maximum level
+	RoleProjectViewer BuiltInRole = "viewer"
+	// RoleProjectSupport support access for the project
+	RoleProjectSupport BuiltInRole = "support"
+)
